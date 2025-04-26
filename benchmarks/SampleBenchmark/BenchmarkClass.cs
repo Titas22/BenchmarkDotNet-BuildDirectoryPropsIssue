51b7d5206733b9ca5e7b2fc8fc6881a9e7a8f7e9
Titas22 <66130422+Titas22@users.noreply.github.com>
2025-04-26 01:24:23 +0100

    set up sample repo

---
+++ benchmarks/SampleBenchmark/BenchmarkClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace SampleBenchmark;
+
+public class BenchmarkClass
+{
+    [GlobalSetup]
+    public void Setup()
+    {
+       // Setup code here, if needed
+    }
+
+    [GlobalCleanup]
+    public void Cleanup()
+    {
+       // Cleanup code here, if needed
+    }
+    [Benchmark]
+    public void Scenario1()
+    {
+        // Implement your benchmark here
+    }
+
+    [Benchmark]
+    public void Scenario2()
+    {
+        // Implement your benchmark here
+    }
+}
---
+++ benchmarks/SampleBenchmark/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace SampleBenchmark;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IConfig config = DefaultConfig.Instance;
+        BenchmarkDotNet.Reports.Summary summary = BenchmarkRunner.Run<BenchmarkClass>(config, args);
+
+        // Use this to select benchmarks from the console:
+        // var summaries = BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
+
+        Console.ReadLine();
+    }
+}
